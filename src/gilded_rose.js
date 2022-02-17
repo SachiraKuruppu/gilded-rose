06600d1beb08e444888d1530d61e1b69652a4785
rsachira <rsachira@live.com>
2022-02-18 12:21:18 +1300

    updateQuality() refactored to use the strategy data pattern

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,3 +1,5 @@
+const { ITEM_TYPES, ItemStrategyManager } = require('./item_strategies')
+
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -10,55 +12,19 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      const strategy = ItemStrategyManager.getStrategy(this.items[i].name);
+      const sellIn_quality = strategy.calculate(this.items[i]);
+      
+      this.items[i].sellIn = sellIn_quality.sellIn;
+      this.items[i].quality = sellIn_quality.quality;
     }
-
-    return this.items;
+    
+    return this.items
   }
+  
 }
 
 module.exports = {
