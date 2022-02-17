82a2003fd000ff9c8fb9e519927f221f491962a2
rsachira <rsachira@live.com>
2022-02-18 08:12:15 +1300

    tests written to verify updateQuality()

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -6,4 +6,98 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  describe("Test updateQuality() for the given requirements", function() {
+
+    const item_names = {
+      decreasing_item_names: ["foo"],
+      increasing_item_names: [
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert",
+      ]
+    };
+
+    it("General: quality and SellIn lowers every day", function(){
+      const sellIn = 10, quality = 12
+
+      const gildedRose = new Shop([new Item("foo", sellIn, quality)]);
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].sellIn).toBeLessThan(sellIn)
+      expect(items[0].quality).toBeLessThan(quality)
+    });
+
+    it("General: when sellIn passed, quality degrades twice as fast", function() {
+      const quality = 10;
+
+      const gildedRose = new Shop(item_names.increasing_item_names.forEach(name => new Item(name, 10, quality)));
+      const gildedRose_0 = new Shop(item_names.increasing_item_names.forEach(name => new Item(name, 0, quality)));
+
+      const items   = gildedRose.updateQuality();
+      const items_0 = gildedRose_0.updateQuality();
+
+      for (let i = 0; i < items.length; i++) {
+        const unexpired_decrease = quality - items[i].quality;
+        const expired_decrease   = quality - items_0[i].quality;
+        expect(expired_decrease).toBe(unexpired_decrease * 2);
+      }
+    });
+
+    it("General: quality of an item is never negative", function() {
+      const gildedRose = new Shop(item_names.decreasing_item_names.forEach(name => new Item(name, 0, 0)));
+      const items = gildedRose.updateQuality();
+
+      items.forEach(item => expect(item.quality).toBeGreaterThanOrEqual(0));
+    });
+
+    it("Aged Brie: increases in quality the older it gets", function() {
+      const quality = 10;
+
+      const gildedRose = new Shop([new Item("Aged Brie", 5, quality)]);
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].quality).toBeGreaterThan(quality);
+    });
+    
+    it("General: quality of an item is never more than 50", function() {
+      const gildedRose = new Shop(item_names.increasing_item_names.forEach(name => new Item(name, 10, 50)));
+      const items = gildedRose.updateQuality();
+
+      items.forEach(item => expect(item.quality).toBe(50));
+    });
+    
+    it("Sullfuras: never has to be sold or decreases in quality", function() {
+      const sellIn = 10, quality = 10;
+
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", sellIn, quality)]);
+      const items = gildedRose.updateQuality();
+
+      expect(items[0].sellIn).toBe(sellIn);
+      expect(items[0].quality).toBe(quality);
+    });
+    
+    it("Backstage passes: increases in quality as the selling value approaches", function() {
+      const sellIns = [15, 10, 5, 0], quality = 10;
+      let items = sellIns.map(sellIn => new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality));
+      const expected_quality = sellIns.map(sellIn => {
+        
+        if (sellIn <= 0)
+          return 0;
+        else if (sellIn <= 5)
+          return quality + 3;
+        else if (sellIn <= 10)
+          return quality + 2;
+        else if (sellIn > 10)
+          return quality + 1;
+      });
+
+      const gildedRose = new Shop(items);
+      items = gildedRose.updateQuality();
+
+      for (let i = 0; i < sellIns.length; i++) {
+        expect(items[i].quality).toBe(expected_quality[i])
+      }
+    });
+
+  });
 });
